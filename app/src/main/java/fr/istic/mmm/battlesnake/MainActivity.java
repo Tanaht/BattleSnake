d571a31389c594bab1e8da5f7c09fa63032d4da4
Unknown <beaulielloic@gmail.com>
2018-02-08 11:56:51 +0100

    add autorization to the manifest and service socket

--- app/src/main/java/fr/istic/mmm/battlesnake/MainActivity.java
+++ app/src/main/java/fr/istic/mmm/battlesnake/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String Tag = "fr.istic.mmm.battlesnake.MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
