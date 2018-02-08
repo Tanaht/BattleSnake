80d853211ccabb0e8ad80728545fc8b8ff8ee282
Tanaky <charp.antoine@gmail.com>
2018-02-08 11:19:44 +0100

    Create MenuBurger and associated Activity

--- app/src/main/java/fr/istic/mmm/battlesnake/MainActivity.java
+++ app/src/main/java/fr/istic/mmm/battlesnake/MainActivity.java
@@ -2,6 +2,9 @@ package fr.istic.mmm.battlesnake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void gotoProfile(MenuItem i) {
+
+    }
+
+    public void gotoRanking(MenuItem i) {
+
+    }
+
+    public void gotoAbout(MenuItem i) {
+
     }
 }
